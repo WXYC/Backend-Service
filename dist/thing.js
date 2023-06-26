04b811e59893f10f1d3deb8ef7763f13ed4c5e7a
Adrian Bruno <alter_me@Adrians-Air.attlocal.net>
2023-06-26 18:56:59 -0400

    not tracking the compiled files

--- dist/thing.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.thingy = void 0;
-function thingy() {
-    console.log('Hey Peeps!');
-}
-exports.thingy = thingy;
