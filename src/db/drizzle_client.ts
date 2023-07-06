42f784b4787a09212d5da75f31b6fbec08e5185c
Adrian Bruno <abruno.heel@gmail.com>
2023-07-06 11:15:29 -0400

    File containing the drizzle db client

---
+++ src/db/drizzle_client.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { PostgresJsDatabase, drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+
+const queryClient = postgres({
+  host: process.env.DB_ENDPOINT,
+  port: process.env.DB_PORT != null ? +process.env.DB_PORT : 5432,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+});
+
+export const db: PostgresJsDatabase = drizzle(queryClient);
