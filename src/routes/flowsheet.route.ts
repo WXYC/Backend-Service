2f3b6b95ff962139a18a4ed787d20869401eab1e
Adrian Bruno <abruno.heel@gmail.com>
2023-07-25 17:26:57 -0400

    altered get route to match current api structure

--- src/routes/flowsheet.route.ts
+++ src/routes/flowsheet.route.ts
@@ -3,6 +3,8 @@ import * as flowsheetController from '../controllers/flowsheet.controller';
 
 export const flowsheet_route = Router();
 
-flowsheet_route.get('/', flowsheetController.get);
+flowsheet_route.get('/recentEntries', flowsheetController.get);
 
 flowsheet_route.get('/latest', flowsheetController.getLatest);
+
+flowsheet_route.post('/', flowsheetController.add_entry);
