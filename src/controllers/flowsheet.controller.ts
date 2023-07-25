a84571d05155385fd786c08699672b233f503e82
Adrian Bruno <abruno.heel@gmail.com>
2023-07-25 17:26:22 -0400

    Fixed guards checking for query params

--- src/controllers/flowsheet.controller.ts
+++ src/controllers/flowsheet.controller.ts
@@ -1,6 +1,6 @@
-import { RequestHandler } from 'express';
+import { Request, RequestHandler } from 'express';
 import { NewFSEntry, FSEntry } from '../db/schema';
-import { getTracks } from '../services/flowsheet.service';
+import { getTracks /*addTrack*/ } from '../services/flowsheet.service';
 
 type QueryParams = {
   page: number;
@@ -12,7 +12,7 @@ type QueryParams = {
 export const get: RequestHandler = async (req, res, next) => {
   console.log('Get Flowsheet Data');
   const query = req.query as unknown as QueryParams;
-  if (query.page && query.limit) {
+  if (!(query.page === undefined || query.limit === undefined)) {
     const offset = query.page * query.limit;
     const limit = query.limit;
 
@@ -57,13 +57,43 @@ export const getLatest: RequestHandler = async (req, res, next) => {
   }
 };
 
-export const add_entry: RequestHandler = async (req, res, next) => {
-  //check for things that MUST be sent by the client
-  if (!(req.body.show_id && req.body.rotation_id && req.body.track_title)) {
-    console.error('Missing required entry parameters parameters');
-    res.status(400);
-    res.send('Missing required entry parameters: show_id, rotation_id, or track_title');
-  } else {
-    res.send('placeholder');
-  }
+export type FSEntryRequestBody = {
+  show_id: number;
+  track_title: string;
+  rotation_id: number;
+  album_id: number;
+  artist_name: string;
+  album_title: string;
+  requst_flag?: boolean;
 };
+
+//either an id is provided (meaning it came from the user's bin or was fuzzy found)
+// or it's not provided in which case whe just throw the data provided into the table w/ album_id 0
+export const add_entry: RequestHandler = async (req: Request<object, object, FSEntryRequestBody>, res, next) => {
+  //check for things that MUST be sent by the client
+  // const body = req.body;
+  // if (body.show_id === undefined || body.rotation_id === undefined || body.track_title === undefined) {
+  //   console.error('Missing required entry parameters');
+  //   res.status(400);
+  //   res.send('Missing required entry parameters: show_id, rotation_id, track_title');
+  // } else if (body.rotation_id === 0 && (body.album_id == undefined || body.album_id === 0) && (!body.album_title)) {
+  //   console.error('Missing album identifier');
+  //   res.send(400);
+  //   res.send('Missing required entry parameter: either album_id or album_title');
+  // } else {
+  //   try {
+  //     const entry_obj = await addTrack(body);
+  //   } catch (e) {
+  //     console.error('Error: Failed to add flowsheet entry');
+  //     console.error(`Error: ${e}`);
+  //   }
+  // }
+  res.status(200);
+  res.send('TODO');
+};
+
+/*Minimum info:
+Always: show_id, rotation_id, track_title
+Sometimes: request_flag
+Album_identifier: rotation_id, album_id, or (album_title & artist_name)
+*/
