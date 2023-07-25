dbbcd7e4ce27297328fb0df8af2672019e7ee282
Adrian Bruno <abruno.heel@gmail.com>
2023-07-25 17:22:34 -0400

    Cleaned up app.ts with new project structure

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,24 @@
 //import fetch, { Response as Fetch_Response } from 'node-fetch';
 import 'dotenv/config';
-import express, { Request, Response } from 'express';
-import * as schema from './db/schema.js';
-//import { sql } from 'drizzle-orm';
-import { db } from './db/drizzle_client.js';
-import { dj_route } from './routes/djs.route.js';
-import { flowsheet_route } from './routes/flowsheet.route.js';
+import express from 'express';
+import { dj_route } from './routes/djs.route';
+import { flowsheet_route } from './routes/flowsheet.route';
+import { library_route } from './routes/library.route';
 
 const port = process.env.PORT;
 const app = express();
 app.use(express.json());
 
-type QueryParams = {
-  page: number;
-  limit: number;
-  start_date: string;
-  end_date: string;
-};
-
-app.use('/flowsheet', flowsheet_route);
-
 /* TODO
 -Accept jwt and verify that user is part of mgmt.
 -Use cognito api to update user store as well.
 */
+
+app.use('/flowsheet', flowsheet_route);
+
 app.use('/djs', dj_route);
+
+app.use('/library', library_route);
 
 app.listen(port, () => {
   console.log(`listening on port: ${port}!`);
