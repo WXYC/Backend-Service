61282318c783481dd9884a39722d6923558ea5a5
Adrian Bruno <abruno.heel@gmail.com>
2023-08-09 20:17:48 -0400

    Made controller names more self descriptive

--- src/routes/library.route.ts
+++ src/routes/library.route.ts
@@ -4,10 +4,10 @@ import * as libraryController from '../controllers/library.controller';
 export const library_route = Router();
 
 //secure: discuss, should we lock down the catalog?
-library_route.get('/', libraryController.get);
+library_route.get('/', libraryController.getAlbum);
 
 //secure: mgmt
-library_route.post('/', libraryController.post);
+library_route.post('/', libraryController.addAlbum);
 
 library_route.get('/get-rotation', libraryController.getRotation);
 
