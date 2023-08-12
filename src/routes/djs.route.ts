7106a618ac340202713968b03522c9c463bb1e24
Adrian Bruno <abruno.heel@gmail.com>
2023-08-12 19:47:22 -0400

    fixed dj registration and data fetching with new schema change

--- src/routes/djs.route.ts
+++ src/routes/djs.route.ts
@@ -3,11 +3,11 @@ import * as djsController from '../controllers/djs.controller';
 
 export const dj_route = Router();
 
+//secure: mgmt & individual dj
+dj_route.get('/', djsController.getDJInfo);
+
 //secure: mgmt
 dj_route.post('/register', djsController.register);
-
-//secure: mgmt & individual dj
-dj_route.get('/info', djsController.info);
 
 dj_route.post('/bin', djsController.addToBin);
 
--- src/services/djs.service.ts
+++ src/services/djs.service.ts
@@ -8,18 +8,15 @@ export const insertDJ = async (new_dj: NewDJ) => {
   return response[0];
 };
 
-export const getDJInfo = async (items: DJQueryParams) /*:Promise<schema.DJ>*/ => {
+export const getDJInfoFromDB = async (items: DJQueryParams) /*:Promise<schema.DJ>*/ => {
   let query_value: number | string;
   let query_col;
   if (items.id !== undefined) {
     query_value = items.id;
     query_col = djs.id;
-  } else if (items.email !== undefined) {
-    query_value = items.email;
-    query_col = djs.email;
-  } else if (items.dj_name !== undefined) {
-    query_value = items.dj_name;
-    query_col = djs.dj_name;
+  } else if (items.cognito_user_name !== undefined) {
+    query_value = items.cognito_user_name;
+    query_col = djs.cognito_user_name;
   } else if (items.real_name !== undefined) {
     query_value = items.real_name;
     query_col = djs.real_name;
@@ -27,16 +24,12 @@ export const getDJInfo = async (items: DJQueryParams) /*:Promise<schema.DJ>*/ =>
     throw new Error('Did not specify a query parameter');
   }
 
-  console.log(query_value);
-  console.log('------------------------');
-  console.log(typeof query_value);
   const dj_obj: DJ[] = await db
     .select()
     .from(djs)
     .where(sql`${query_col} = ${query_value}`);
 
-  //console.log(dj_obj);
-  return dj_obj;
+  return dj_obj[0];
 };
 
 export const addToBin = async (bin_entry: NewBinEntry): Promise<BinEntry> => {
