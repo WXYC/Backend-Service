e5c4637b90a092e8f73ff5df2a82132e756c320b
Adrian Bruno <abruno.heel@gmail.com>
2023-08-12 19:47:22 -0400

    fixed dj registration and data fetching with new schema change

--- src/controllers/djs.controller.ts
+++ src/controllers/djs.controller.ts
@@ -2,18 +2,23 @@ import { RequestHandler } from 'express';
 import * as DJService from '../services/djs.service';
 import { DJ, NewDJ, NewBinEntry } from '../db/schema';
 
-export const register: RequestHandler = async (req, res, next) => {
+export type DJQueryParams = {
+  id?: number;
+  cognito_user_name?: string;
+  real_name?: string;
+};
+
+export const register: RequestHandler<object, unknown, DJQueryParams> = async (req, res, next) => {
   console.log('registering new user');
   console.log(req.body);
-  if (!(req.body.real_name && req.body.dj_name && req.body.email)) {
+  if (req.body.cognito_user_name === undefined) {
     console.log('Bad Request: Missing New DJ Parameters');
     res.status(400);
     res.send('Bad Request: Missing New DJ Parameters');
   } else {
     const new_dj: NewDJ = {
+      cognito_user_name: req.body.cognito_user_name,
       real_name: req.body.real_name,
-      dj_name: req.body.dj_name,
-      email: req.body.email,
     };
 
     try {
@@ -21,37 +26,34 @@ export const register: RequestHandler = async (req, res, next) => {
       res.status(200).json(dj_obj);
     } catch (e) {
       console.error(`Failed To Create DJ`);
-      console.error(`Error: ${e}`);
+      console.error(e);
       next(e);
-      // res.status(500);
-      // res.send('Server Error: Failed to create DJ');
     }
   }
 };
 
-export type DJQueryParams = {
-  id: number;
-  email: string;
-  dj_name: string;
-  real_name: string;
-};
-
-export const info: RequestHandler<object, unknown, object, DJQueryParams> = async (req, res, next) => {
+export const getDJInfo: RequestHandler<object, unknown, object, DJQueryParams> = async (req, res, next) => {
   const query = req.query;
-  try {
-    const dj_info: DJ[] = await DJService.getDJInfo(query);
-    if (dj_info.length) {
-      console.log(dj_info[0]);
-      res.status(200);
-      res.send(dj_info);
-    } else {
-      console.error('DJ not found');
-      res.status(404).send('DJ not found');
+
+  if (query.cognito_user_name === undefined && query.id === undefined) {
+    console.error('Error, Missing DJ Identifier: cognito_user_name or id');
+    res.send(400);
+  } else {
+    try {
+      const dj_info: DJ = await DJService.getDJInfoFromDB(query);
+      if (dj_info !== undefined) {
+        console.log(dj_info);
+        res.status(200);
+        res.send(dj_info);
+      } else {
+        console.error('DJ not found');
+        res.status(404).send('DJ not found');
+      }
+    } catch (e) {
+      console.error('Error looking up DJ');
+      console.error(e);
+      next(e);
     }
-  } catch (e) {
-    console.error('Error looking up DJ');
-    console.error(`Error: ${e}`);
-    next(e);
   }
 };
 
@@ -64,8 +66,8 @@ export type binQuery = {
 
 export const addToBin: RequestHandler<object, unknown, object, binQuery> = async (req, res, next) => {
   if (req.query.album_id === undefined || req.query.dj_id === undefined) {
-    console.error('Error Missing Album Identifier: album_id');
-    res.send(400).send('Error Missing DJ or album identifier: dj_id or album_id');
+    console.error('Bad Request, Missing Album Identifier: album_id');
+    res.send(400).send('Bad Request, Missing DJ or album identifier: album_id');
   } else {
     const bin_entry: NewBinEntry = {
       dj_id: req.query.dj_id,
@@ -85,8 +87,8 @@ export const addToBin: RequestHandler<object, unknown, object, binQuery> = async
 
 export const deleteFromBin: RequestHandler<object, unknown, object, binQuery> = async (req, res, next) => {
   if (req.query.bin_entry_id === undefined || req.query.dj_id) {
-    console.error('Error, Missing Bin Entry Identifier: bin_entry_id');
-    res.send(400).send('Error, Missing Bin Entry Identifier: bin_entry_id');
+    console.error('Bad Request, Missing Bin Entry Identifier: bin_entry_id');
+    res.send(400).send('Bad Request, Missing Bin Entry Identifier: bin_entry_id');
   } else {
     try {
       //check that the dj_id === dj_id of bin entry
