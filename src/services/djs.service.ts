2d0c055c933dec3b581f42b45fc8ecd4f94494ce
Adrian Bruno <abruno.heel@gmail.com>
2023-08-13 01:34:40 -0400

    Feature: /djs/bin GET endpoint complete

--- src/services/djs.service.ts
+++ src/services/djs.service.ts
@@ -1,5 +1,5 @@
 import { db } from '../db/drizzle_client';
-import { NewDJ, DJ, djs, bins, NewBinEntry, BinEntry } from '../db/schema';
+import { NewDJ, DJ, djs, bins, NewBinEntry, BinEntry, library, artists } from '../db/schema';
 import { sql, eq, and } from 'drizzle-orm';
 import { DJQueryParams } from '../controllers/djs.controller';
 
@@ -43,4 +43,20 @@ export const removeFromBin = async (bin_entry_id: number, dj_id: number): Promis
     .where(and(eq(bins.dj_id, dj_id), eq(bins.id, bin_entry_id)))
     .returning();
   return removed_bin_entry[0];
+};
+
+export const getBinFromDB = async (dj_id: number) => {
+  const dj_bin = await db
+    .select({
+      album_id: bins.album_id,
+      album_title: library.album_title,
+      artist_name: artists.artist_name,
+      track_title: bins.track_title,
+    })
+    .from(bins)
+    .innerJoin(library, eq(bins.album_id, library.id))
+    .innerJoin(artists, eq(library.artist_id, artists.id))
+    .where(eq(bins.dj_id, dj_id));
+
+  return dj_bin;
 };
