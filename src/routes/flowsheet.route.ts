fe0680b8867df25038a274c16f9d39977ee3763f
Adrian Bruno <abruno.heel@gmail.com>
2023-10-20 10:58:44 -0400

    Added auth middleware to select flowsheet endpoints

--- src/routes/flowsheet.route.ts
+++ src/routes/flowsheet.route.ts
@@ -1,21 +1,22 @@
 import { Router } from 'express';
+import { cognitoMiddleware } from '../middleware/cognito.auth';
 import * as flowsheetController from '../controllers/flowsheet.controller';
 
 export const flowsheet_route = Router();
 
 flowsheet_route.get('/', flowsheetController.getEntries);
 
-flowsheet_route.post('/', flowsheetController.addEntry);
+flowsheet_route.post('/', cognitoMiddleware, flowsheetController.addEntry);
 
-flowsheet_route.patch('/', flowsheetController.updateEntry);
+flowsheet_route.patch('/', cognitoMiddleware, flowsheetController.updateEntry);
 
-flowsheet_route.delete('/', flowsheetController.deleteEntry);
+flowsheet_route.delete('/', cognitoMiddleware, flowsheetController.deleteEntry);
 
 flowsheet_route.get('/latest', flowsheetController.getLatest);
 
-flowsheet_route.post('/join', flowsheetController.joinShow);
+flowsheet_route.post('/join', cognitoMiddleware, flowsheetController.joinShow);
 
-flowsheet_route.post('/end', flowsheetController.leaveShow);
+flowsheet_route.post('/end', cognitoMiddleware, flowsheetController.leaveShow);
 
 flowsheet_route.get('/djs-on-air', flowsheetController.getDJList);
 
