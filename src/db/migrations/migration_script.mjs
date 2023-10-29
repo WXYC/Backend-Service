bbae59f6c4f66bdfa24ab361e46fa8da6e01515c
Adrian Bruno <abruno.heel@gmail.com>
2023-10-29 12:06:59 -0400

    semantic variable name change

--- src/db/migrations/migration_script.mjs
+++ src/db/migrations/migration_script.mjs
@@ -3,14 +3,14 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 
-const sql = postgres({
+const queryClient = postgres({
   host: process.env.DB_ENDPOINT,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
 });
-const db = drizzle(sql);
+const db = drizzle(queryClient);
 
 migrate(db, { migrationsFolder: 'src/db/migrations' })
   .catch((e) => console.error(e))
