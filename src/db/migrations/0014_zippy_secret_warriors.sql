d8ac64a6cfe78752cecaef8e7c794c9610bd64d1
Adrian Bruno <abruno.heel@gmail.com>
2023-11-10 08:39:04 -0500

    Fixing some issues with our single source of truth DB migrations

--- src/db/migrations/0014_zippy_secret_warriors.sql
+++ src/db/migrations/0014_zippy_secret_warriors.sql
@@ -1,11 +1,9 @@
 CREATE TABLE IF NOT EXISTS "wxyc_schema"."show_djs" (
-	"id" serial PRIMARY KEY NOT NULL,
 	"show_id" integer NOT NULL,
 	"dj_id" integer NOT NULL,
-	"active" boolean DEFAULT false NOT NULL
+	"time_joined" timestamp,
+	"time_left" timestamp
 );
---> statement-breakpoint
-ALTER TABLE "wxyc_schema"."flowsheet" DROP CONSTRAINT "flowsheet_show_id_shows_id_fk";
 --> statement-breakpoint
 ALTER TABLE "wxyc_schema"."shows" DROP CONSTRAINT "shows_dj_id_djs_id_fk";
 --> statement-breakpoint
@@ -13,22 +11,18 @@ ALTER TABLE "wxyc_schema"."shows" DROP CONSTRAINT "shows_dj_id2_djs_id_fk";
 --> statement-breakpoint
 ALTER TABLE "wxyc_schema"."shows" DROP CONSTRAINT "shows_dj_id3_djs_id_fk";
 --> statement-breakpoint
+ALTER TABLE "wxyc_schema"."flowsheet" ALTER COLUMN "show_id" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "wxyc_schema"."flowsheet" ALTER COLUMN "track_title" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "wxyc_schema"."flowsheet" ALTER COLUMN "album_title" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "wxyc_schema"."flowsheet" ALTER COLUMN "artist_name" DROP NOT NULL;--> statement-breakpoint
 ALTER TABLE "wxyc_schema"."djs" ADD COLUMN "dj_name" varchar;--> statement-breakpoint
-ALTER TABLE "wxyc_schema"."shows" ADD COLUMN "flowsheet_start_index" integer;--> statement-breakpoint
-ALTER TABLE "wxyc_schema"."shows" ADD COLUMN "flowsheet_end_index" integer;--> statement-breakpoint
+ALTER TABLE "wxyc_schema"."shows" ADD COLUMN "primary_dj_id" integer;--> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "wxyc_schema"."shows" ADD CONSTRAINT "shows_flowsheet_start_index_flowsheet_id_fk" FOREIGN KEY ("flowsheet_start_index") REFERENCES "wxyc_schema"."flowsheet"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "wxyc_schema"."shows" ADD CONSTRAINT "shows_primary_dj_id_djs_id_fk" FOREIGN KEY ("primary_dj_id") REFERENCES "wxyc_schema"."djs"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "wxyc_schema"."shows" ADD CONSTRAINT "shows_flowsheet_end_index_flowsheet_id_fk" FOREIGN KEY ("flowsheet_end_index") REFERENCES "wxyc_schema"."flowsheet"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-ALTER TABLE "wxyc_schema"."flowsheet" DROP COLUMN IF EXISTS "show_id";--> statement-breakpoint
 ALTER TABLE "wxyc_schema"."shows" DROP COLUMN IF EXISTS "dj_id";--> statement-breakpoint
 ALTER TABLE "wxyc_schema"."shows" DROP COLUMN IF EXISTS "dj_id2";--> statement-breakpoint
 ALTER TABLE "wxyc_schema"."shows" DROP COLUMN IF EXISTS "dj_id3";--> statement-breakpoint
