9c569d299929901ea0b46f70a39b4680d909e414
Adrian Bruno <abruno.heel@gmail.com>
2024-05-28 23:13:57 -0400

    Updated env variable to be more accurate and descriptive

--- src/db/drizzle_client.ts
+++ src/db/drizzle_client.ts
@@ -3,7 +3,7 @@ import { PostgresJsDatabase, drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
 const queryClient = postgres({
-  host: process.env.DB_ENDPOINT,
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT != null ? +process.env.DB_PORT : 5432,
   database: process.env.DB_NAME,
   username: process.env.DB_USERNAME,
