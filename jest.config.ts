534db8edd90025cb96e3d84e28870c36bbccdc42
Adrian Bruno <abruno.heel@gmail.com>
2024-06-23 17:59:36 -0400

    Jest + supertest setup

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from 'jest';
+
+const config: Config = {
+  // The glob patterns Jest uses to detect test files
+  testMatch: ['**/tests/specs/?(*.)+(spec).js'],
+  reporters: [
+    'default',
+    [
+      'jest-html-reporters',
+      {
+        publicPath: './tests/report', // report directory
+        filename: 'report.html', // report file name
+        pageTitle: 'SuperTest and Jest API Test Report', // report title
+        overwrite: true, // enable report file overwrite
+        expand: true, // enable report file expansion
+      },
+    ],
+  ],
+};
+
+export default config;
