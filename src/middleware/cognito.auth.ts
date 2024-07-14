24b445a0d72599f2536b6dd6991814d81f1124c0
Adrian Bruno <abruno.heel@gmail.com>
2024-07-14 16:50:12 -0400

    Added some comments showing how to disable auth for offline testing.

--- src/middleware/cognito.auth.ts
+++ src/middleware/cognito.auth.ts
@@ -12,14 +12,17 @@ export const cognitoMiddleware = (permissionsLevel: string | null = null) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const accessToken = req.header('Authorization') || '';
     try {
+      //comment this out to bypass auth when offline testing
       const verification = await jwtVerifier.verify(accessToken); //throws error for invalid tokens
-
       if (permissionsLevel !== null && !verification['cognito:groups']?.includes(permissionsLevel)) {
         res.status(403).json({ status: 403, message: 'Forbidden' });
       } else {
         res.locals.decodedJWT = verification;
         next();
       }
+
+      //uncomment this to bypass auth when offline testing
+      // next();
     } catch (e) {
       res.status(403).json({ status: 403, message: 'Forbidden' });
     }
