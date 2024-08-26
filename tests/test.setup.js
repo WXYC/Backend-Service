e5f71e7f244dbf3ddf6e21734786af0d2ac5407b
Adrian Bruno <abruno.heel@gmail.com>
2024-08-25 21:12:09 -0400

    Added auth bypass flag to test setup

--- tests/test.setup.js
+++ tests/test.setup.js
@@ -1,3 +1,4 @@
+require('dotenv').config({ path: `${__dirname}/../.env` });
 const get_access_token = require('./utils/cognito_auth');
 
 global.primary_dj_id = 1;
@@ -5,5 +6,9 @@ global.secondary_dj_id = 2;
 global.access_token = '';
 
 beforeAll(async () => {
-  global.access_token = await get_access_token();
+  if (process.env.AUTH_BYPASS === 'true') {
+    global.access_token = 'Auth Bypass Enabled';
+  } else {
+    global.access_token = await get_access_token();
+  }
 });
