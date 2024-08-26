8784d7aa5cb81d87401efce94dd23c4a081309b7
Adrian Bruno <abruno.heel@gmail.com>
2024-08-25 20:42:39 -0400

    Added Auth bypass configured in .env

--- src/middleware/cognito.auth.ts
+++ src/middleware/cognito.auth.ts
@@ -11,20 +11,21 @@ export const jwtVerifier = CognitoJwtVerifier.create({
 export const cognitoMiddleware = (permissionsLevel: string | null = null) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const accessToken = req.header('Authorization') || '';
-    try {
-      //comment this out to bypass auth when offline testing
-      const verification = await jwtVerifier.verify(accessToken); //throws error for invalid tokens
-      if (permissionsLevel !== null && !verification['cognito:groups']?.includes(permissionsLevel)) {
+    if (process.env.AUTH_BYPASS === 'true') {
+      res.locals.decodedJWT = { username: process.env.AUTH_USERNAME };
+      next();
+    } else {
+      try {
+        const verification = await jwtVerifier.verify(accessToken); //throws error for invalid tokens
+        if (permissionsLevel !== null && !verification['cognito:groups']?.includes(permissionsLevel)) {
+          res.status(403).json({ status: 403, message: 'Forbidden' });
+        } else {
+          res.locals.decodedJWT = verification;
+          next();
+        }
+      } catch (e) {
         res.status(403).json({ status: 403, message: 'Forbidden' });
-      } else {
-        res.locals.decodedJWT = verification;
-        next();
       }
-
-      //uncomment this to bypass auth when offline testing
-      // next();
-    } catch (e) {
-      res.status(403).json({ status: 403, message: 'Forbidden' });
     }
   };
 };
