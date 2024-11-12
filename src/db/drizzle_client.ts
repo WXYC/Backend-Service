6199240359ded4b3c1ec9f877c4e4db60463b54f
Adrian Bruno <abruno.heel@gmail.com>
2024-11-11 22:39:24 -0500

    Simplifies the logic surrounding the USE_CI env variable flag

--- src/db/drizzle_client.ts
+++ src/db/drizzle_client.ts
@@ -4,7 +4,7 @@ import postgres from 'postgres';
 
 const queryClient = postgres({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT != null ? +process.env.DB_PORT : 5432,
+  port: process.env.DB_PORT != null ? Number(process.env.DB_PORT) : 5432,
   database: process.env.DB_NAME,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
