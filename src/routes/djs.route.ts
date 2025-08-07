1c33e2c78dd1ef48038973383f1026ab71955124
Adrian Bruno <abruno.heel@gmail.com>
2025-08-07 16:25:35 -0400

    Add support for multiple roles on an individual endpoint. Also allow for open auth where we store token if it exists to be handled later, but allow all connections through.

--- src/routes/djs.route.ts
+++ src/routes/djs.route.ts
@@ -1,20 +1,22 @@
 import { Router } from 'express';
 import * as djsController from '../controllers/djs.controller.js';
-import { cognitoMiddleware } from '../middleware/cognito.auth.js';
+import { cognitoMiddleware, Roles } from '../middleware/cognito.auth.js';
+
+const { dj, mgmt } = Roles;
 
 export const dj_route = Router();
 
 //TODO: secure - mgmt & individual dj
-dj_route.get('/', cognitoMiddleware(), djsController.getDJInfo);
+dj_route.get('/', cognitoMiddleware(dj), djsController.getDJInfo);
 
-dj_route.post('/register', cognitoMiddleware('station-management'), djsController.register);
+dj_route.post('/register', cognitoMiddleware(mgmt), djsController.register);
 
-dj_route.patch('/register', cognitoMiddleware(), djsController.update);
+dj_route.patch('/register', cognitoMiddleware(dj), djsController.update);
 
-dj_route.post('/bin', cognitoMiddleware(), djsController.addToBin);
+dj_route.post('/bin', cognitoMiddleware(dj), djsController.addToBin);
 
-dj_route.delete('/bin', cognitoMiddleware(), djsController.deleteFromBin);
+dj_route.delete('/bin', cognitoMiddleware(dj), djsController.deleteFromBin);
 
-dj_route.get('/bin', cognitoMiddleware(), djsController.getBin);
+dj_route.get('/bin', cognitoMiddleware(dj), djsController.getBin);
 
-dj_route.get('/playlists', cognitoMiddleware(), djsController.getPlaylistsForDJ);
+dj_route.get('/playlists', cognitoMiddleware(dj), djsController.getPlaylistsForDJ);
