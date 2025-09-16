9fcf454ae6ca25502e5930af02d2811b7f7588f3
Adrian Bruno <abruno.heel@gmail.com>
2025-09-16 07:55:19 -0400

    Update default name of WxycError

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,6 +1,6 @@
 export default class WxycError extends Error {
   statusCode: number;
-  constructor(message: string, statusCode: number = 500, name: string = 'ClientError') {
+  constructor(message: string, statusCode: number = 500, name: string = 'WxycError') {
     super(message);
     this.name = name;
     this.statusCode = statusCode;
