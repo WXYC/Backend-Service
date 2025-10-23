58e5b2f935db0327237948e51e45638a3e13712f
Jackson Meade <jacksonmeade@outlook.com>
2025-10-22 22:12:13 -0400

    Use dotenvx for injection of local env vars

--- apps/backend/tsup.config.ts
+++ apps/backend/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig((options) => ({
   entry: ['app.ts'],
-  format: ["esm"],
+  format: ["cjs"],
   outDir: 'dist',
   clean: true,
   onSuccess: options.watch ? 'node ./dist/app.js' : undefined,
