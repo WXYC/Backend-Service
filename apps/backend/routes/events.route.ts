1fa150db8129a4e7a944d65fc8733666bbacf78d
Adrian Bruno <abruno.heel@gmail.com>
2026-02-27 23:14:08 -0500

    remove auth on test event trigger route that caused integration test failures

--- apps/backend/routes/events.route.ts
+++ apps/backend/routes/events.route.ts
@@ -4,9 +4,11 @@ import * as serverEvents from '../controllers/events.conroller.js';
 
 export const events_route = Router();
 
-//TODO: secure - mgmt & individual dj
+//TODO: You shouldn't have to be authenticated to register an event client.
+// Each topic has it's own permissions, some of which are public.
 events_route.post('/register', requirePermissions({ flowsheet: ['read'] }), serverEvents.registerEventClient);
 
+//TODO: You shouldn't have to be authenticated to subscribe to a topic
 events_route.put('/subscribe', requirePermissions({ flowsheet: ['read'] }), serverEvents.subscribeToTopic);
 
-events_route.get('/test', requirePermissions({ flowsheet: ['read'] }), serverEvents.testTrigger);
+events_route.get('/test', serverEvents.testTrigger);
