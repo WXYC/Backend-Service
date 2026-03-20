1670e6f53fbf9ed272ab1a85bb53e1958e793c8d
Jake Bromberg <jake@funlandresearch.com>
2026-03-19 17:35:34 -0700

    style: format file-naming-conventions test with Prettier

--- tests/unit/naming/file-naming-conventions.test.ts
+++ tests/unit/naming/file-naming-conventions.test.ts
@@ -10,15 +10,12 @@ const NAMING_CONVENTIONS: Record<string, RegExp> = {
 };
 
 describe('file naming conventions', () => {
-  it.each(Object.entries(NAMING_CONVENTIONS))(
-    '%s/ files should match %s',
-    (directory, pattern) => {
-      const dirPath = path.join(BACKEND_DIR, directory);
-      const files = fs.readdirSync(dirPath, { withFileTypes: true });
-      const tsFiles = files.filter((f) => f.isFile() && f.name.endsWith('.ts'));
-      const violations = tsFiles.filter((f) => !pattern.test(f.name));
+  it.each(Object.entries(NAMING_CONVENTIONS))('%s/ files should match %s', (directory, pattern) => {
+    const dirPath = path.join(BACKEND_DIR, directory);
+    const files = fs.readdirSync(dirPath, { withFileTypes: true });
+    const tsFiles = files.filter((f) => f.isFile() && f.name.endsWith('.ts'));
+    const violations = tsFiles.filter((f) => !pattern.test(f.name));
 
-      expect(violations.map((f) => f.name)).toEqual([]);
-    },
-  );
+    expect(violations.map((f) => f.name)).toEqual([]);
+  });
 });
