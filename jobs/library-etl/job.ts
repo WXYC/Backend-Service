df7d5e301dd9aa4ebd855235f4b3b36b97f43e9a
Jake Bromberg <jake@funlandresearch.com>
2026-04-22 14:09:39 -0700

    fix: sync crossref comment updates instead of dropping them
    artist_crossreference and artist_library_crossreference imports used
    onConflictDoNothing, silently discarding comment changes from
    tubafrenzy. Now uses onConflictDoUpdate to sync comment edits.

--- jobs/library-etl/job.ts
+++ jobs/library-etl/job.ts
@@ -625,7 +625,10 @@ const importArtistCrossRefs = async (
         target_artist_id: targetId,
         comment: row.comment,
       })
-      .onConflictDoNothing();
+      .onConflictDoUpdate({
+        target: [artist_crossreference.source_artist_id, artist_crossreference.target_artist_id],
+        set: { comment: sql`excluded.comment` },
+      });
 
     imported++;
   }
@@ -761,7 +764,10 @@ const importReleaseCrossRefs = async (
         library_id: albumId,
         comment: row.comment,
       })
-      .onConflictDoNothing();
+      .onConflictDoUpdate({
+        target: [artist_library_crossreference.artist_id, artist_library_crossreference.library_id],
+        set: { comment: sql`excluded.comment` },
+      });
 
     imported++;
   }
